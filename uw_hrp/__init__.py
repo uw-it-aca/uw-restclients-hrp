38bcc2cdb032398ef51ad42d3790e2f60bf62d2c
Fang Lin <fanglin@uw.edu>
2019-05-28 11:20:59 -0700

    Changed to initialize HRP_DAO object after configuration settings are ready

--- uw_hrp/__init__.py
+++ uw_hrp/__init__.py
@@ -9,11 +9,10 @@ from restclients_core.exceptions import DataFailureException
 
 
 logger = logging.getLogger(__name__)
-hrp_dao = HRP_DAO()
 
 
 def get_resource(url):
-    response = hrp_dao.getURL(url, {'Accept': 'application/json'})
+    response = HRP_DAO().getURL(url, {'Accept': 'application/json'})
 
     logger.debug("{0} ==status==> {1}".format(url, response.status))
     if response.status != 200:
