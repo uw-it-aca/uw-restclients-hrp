7d646f979384995b9e8a137010b2a3f88457b4f3
Jim Laney <jlaney@uw.edu>
2021-05-25 08:32:52 -0700

    move to github-actions

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ See the README on `GitHub
 <https://github.com/uw-it-aca/uw-restclients-hrp>`_.
 """
 
-# The VERSION file is created by travis-ci, based on the tag name
 version_path = 'uw_hrp/VERSION'
 VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
 VERSION = VERSION.replace("\n", "")
@@ -22,7 +21,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'UW-RestClients-Core<2.0',
+        'UW-RestClients-Core',
         'python-dateutil'
     ],
     license='Apache License, Version 2.0',
