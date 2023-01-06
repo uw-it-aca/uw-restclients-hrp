f7097a4aa40621ea794d2003820a71729b6805ff
Fang Lin <fanglin@uw.edu>
2023-01-06 12:54:10 -0800

    Qa (#26)
    Support hrpws v3. Update copyright year, python version, action versions. Refactor to class.
    
    
    Co-authored-by: Jim Laney <jlaney@uw.edu>

--- uw_hrp/tests/test_dao.py
+++ uw_hrp/tests/test_dao.py
@@ -1,5 +1,6 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
+
 
 from unittest import TestCase
 from uw_hrp.dao import HRP_DAO
